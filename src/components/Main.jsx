d94a42be22af2c5b6a2dc612eb71022e478ded74
Tony Baldessari <tonybalde@hotmail.com>
2023-11-27 15:29:07 -0300

    change bg image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn} from "react-icons/fa"
 function Main() {
   return (
     <div id="main">
-      <img className="w-full h-screen object-cover object-left scale-x-[-1] " src="https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1143&q=80" alt="" />
+      <img className="w-full h-screen object-cover object-left scale-x-[-1] " src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
                 <h1 className="sm:text-5xl text-4xl font-bold text-gray-800  ">I'm Tony Baldessari</h1>
