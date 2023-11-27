5a011b8fd8f23ae0bc63dfd11aa85b7a56fb0e57
Tony Baldessari <tonybalde@hotmail.com>
2023-11-27 15:42:26 -0300

    fix title color in Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,8 +8,8 @@ function Main() {
       <img className="w-full h-screen object-cover object-left scale-x-[-1] " src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-                <h1 className="sm:text-5xl text-4xl font-bold text-gray-800  ">I'm Tony Baldessari</h1>
-                <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+                <h1 className="sm:text-5xl text-4xl font-bold text-black  ">I'm Tony Baldessari</h1>
+                <h2 className="flex sm:text-3xl text-2xl pt-4 text-black">
                 I'm a
                 <TypeAnimation
                     sequence={[
