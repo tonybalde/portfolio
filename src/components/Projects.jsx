d5b924dc77a18c16b1040b9981b5cee0a6d32fda
Tony Baldessari <tonybalde@hotmail.com>
2024-01-03 00:07:22 -0300

    Added a text to Icons when Hover in Desktop mode

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,7 @@ const Projects = () => {
   return (
     <div id="projects" className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'> 
         <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+        <hr />
         <p className="text-center py-8">These are some of the projects</p>
         <div className="grid sm:grid-cols-2 gap-12">
             <ProjectItem img={cryptoImg} title="Crypto App"/>
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -51,6 +51,9 @@ const Sidenav = () => {
         <div className="flex flex-col">
           <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
             <AiOutlineHome size={20} />
+            <span className="text-black text-lg font-bold opacity-0 transition-opacity absolute top-1/2 transform -translate-y-1/2 invisible whitespace-nowrap" style={{ left: '62px' }}>
+              {'< Home />'}
+            </span>
           </a>
           <a href="#projects" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
           <GrProjects size={20} />
