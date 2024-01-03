bcba4cdfc8061b6600c3a1aeca18035712c29e03
Tony Baldessari <tonybalde@hotmail.com>
2024-01-03 00:21:37 -0300

    Added repo and live btn to the project card

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -8,10 +8,15 @@ const ProjectItem = ({img, title}) => {
             <h3 className="text-2xl font-bold text-white tracking-wider text-center">
                 {title}
             </h3>
-            <p className="pb-4 pt-2 text-white text-center">React JS</p>
-            <a href="/">
-                <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">More Info</p>
-            </a>
+            <p className="pb-4 pt-2 text-white text-center">Build with ReactJS, Express, MongoDB, etc..</p>
+            <div class="flex gap-2 justify-center">
+              <a href="/">
+                  <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-sm">Repo</p>
+              </a>
+              <a href="/">
+                  <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-sm">Live</p>
+              </a>
+            </div>            
         </div>
     </div>
   )
