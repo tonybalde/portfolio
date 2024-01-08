be162febb7374bac28a74904506978768f82123d
Tony Baldessari <tonybalde@hotmail.com>
2024-01-08 16:19:55 -0300

    fix path to profile img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ function Main() {
       <img className="w-full h-screen object-cover object-left scale-x-[-1] " src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-                <img src="../../public/tony.jpg" alt="Tony Baldessari" className="profile-img border-black border-4" />
+                <img src="tony.jpg" alt="Tony Baldessari" className="profile-img border-black border-4" />
                 <h1 className="sm:text-5xl text-4xl font-bold text-black text-center ">Hi!👋 I'm Tony Baldessari</h1>
                 <h2 className="flex sm:text-3xl text-2xl pt-4 text-black">
                 I'm a
