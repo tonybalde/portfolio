a985919c90acca43e1b3021baad41a505dc2c3c1
Tony Baldessari <tonybalde@hotmail.com>
2024-01-08 16:59:36 -0300

    Fix Image size

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ function Main() {
             <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
                 <img src="tony.jpg" alt="Tony Baldessari" className="profile-img border-black border-4" />
                 <h1 className="sm:text-5xl text-4xl font-bold text-black text-center title-txt">Hi!👋 I'm Tony Baldessari</h1>
-                <h2 className="flex sm:text-3xl text-2xl pt-4 text-black">
+                <h2 className="flex sm:text-3xl text-2xl pt-4 text-black typing">
                 I'm a
                 <TypeAnimation
                     sequence={[
