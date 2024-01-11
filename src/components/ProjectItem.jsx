774e40994cd072e62674aa41f5a89625219ab932
Tony Baldessari <tonybalde@hotmail.com>
2024-01-11 15:50:03 -0300

    Fix margin and title in projec cards

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -7,11 +7,11 @@ const ProjectItem = ({img, title, description, repo, live}) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] cursor-pointer ">
         <img src={img} alt="/" className="rounded-xl group-hover:opacity-10 " />
-        <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-            <h3 className="text-2xl font-bold text-white tracking-wider text-center">
+        <div className="hidden group-hover:block absolute top-[50%] left-[30%] translate-x-[-20%] translate-y-[-50%] ">
+            <h3 className="text-xl font-bold text-white tracking-wider text-center">
                 {title}
             </h3>
-            <p className="pb-1 pt-1 text-sm text-white text-center">{description}</p>
+            <p className="pb-3 pt-1 text-sm text-white text-center">{description}</p>
             <div class="flex gap-2 justify-center">
               <a href={repo} target='_blank'>
                   <p className="flex gap-2 text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-sm"><FaGithub size={18} /> Repo</p>
